4c8a05cd82d5a292225b9e25ab4b94b4deafdb75
Chungwon Jung <wjdcjddnjs1234@gmail.com>
2026-01-28 22:34:40 +0900

    변경

---
+++ src/main/java/org/example/OppaRecipeApplication.java
@@ -0,0 +1,12 @@
+package org.example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class OppaRecipeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OppaRecipeApplication.class, args);
+    }
+}
