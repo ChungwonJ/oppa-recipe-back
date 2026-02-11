8be954a84d354f0265983ec71471205ee08ff083
Chungwon Jung <wjdcjddnjs1234@gmail.com>
2026-02-12 01:07:37 +0900

    ref) EntityGraph

--- src/main/java/org/example/domain/recipe/repository/RecipeRepository.java
+++ src/main/java/org/example/domain/recipe/repository/RecipeRepository.java
@@ -3,11 +3,13 @@ package org.example.domain.recipe.repository;
 import org.example.domain.recipe.entity.Recipe;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    @EntityGraph(attributePaths = {"user"})
     Page<Recipe> findAllByUserId(Long userId, Pageable pageable);
     Optional<Recipe> findById(Long id);
 }
